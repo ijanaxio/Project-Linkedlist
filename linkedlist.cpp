c75850dd7e88a05ad1819875081739543b08601a
ijanaxio <ijanzikrim@gmail.com>
2025-06-25 14:59:32 +0700

    Menambahkan fungsi tambahPesan untuk menambah pesan ke akhir linked list

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -7,4 +7,16 @@ struct Pesan {
 };
 
 Pesan* head = NULL;
-
+void tambahPesan(string isi) {
+    Pesan* baru = new Pesan{isi, NULL};
+    if (head == NULL) {
+        head = baru;
+    } else {
+        Pesan* temp = head;
+        while (temp->next != NULL) {
+            temp = temp->next;
+        }
+        temp->next = baru;
+    }
+    cout << "Pesan ditambahkan: \"" << isi << "\"\n";
+}
